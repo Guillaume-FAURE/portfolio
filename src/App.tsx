398c02cf36404a41948c1c4dfd8ef23bb3cc2cf6
Guillaume-FAURE <faureg0717@gmail.com>
2022-03-09 17:37:35 +0100

    feat(Presentation): add scroll and different pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import './App.css';
 import pp from './assets/pp.png'
 import { Presentation } from './components/Presentation';
+import { Page } from './components/Page';
 
 function App() {
   return (
-    <Presentation name='Guillaume FAURE' pp={pp}/>
+    <React.Fragment>
+      <Presentation name='Guillaume FAURE' pp={pp}/>
+      <Page name='Projects'></Page>
+      <Page name='Technos'></Page>
+      <Page name='AboutMe'></Page>
+    </React.Fragment>
   )
 }
 
--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -1,6 +1,7 @@
-import * as React from "react";
+import React from "react";
 import { Emoji } from "./Emoji";
-import Pdf from '../assets/Resume-GuillaumeFAURE-eng.pdf';
+import Pdf from '../assets/resume.pdf';
+import { Link } from 'react-scroll';
 
 type AppProps = {
     name: string;
@@ -10,24 +11,26 @@ type AppProps = {
 export function Presentation({name,pp}: AppProps){
     return(
     <React.Fragment>
-        <div id="wrapperPres">
-            <h1>PortFolio {name}</h1>
-            <div id="titlePF">
-                <p id="presentationText"> Hello <Emoji symbol='👋' label="shakingHand" className='animatedHand'/><br/> I'm currently a junior web developper full stack in 2nd year at the IMT-Nord-Europe in France <Emoji symbol='💻' label='laptop' className=''/></p>
+        <div className="Page">
+            <div id="wrapperPres">
+                <h1>PortFolio {name}</h1>
+                <div id="titlePF">
+                    <p id="presentationText"> Hello <Emoji symbol='👋' label="shakingHand" className='animatedHand'/><br/> I'm currently a junior web developper full stack in 2nd year at the IMT-Nord-Europe in France <Emoji symbol='💻' label='laptop' className=''/></p>
+                </div>
+                <div id="contentsTable">
+                <h2>Contents</h2>
+                    <ul id="contentsList">
+                        <li><Link to="Projects" smooth={true} spy={true} duration={100} className="contents">Projects</Link></li>
+                        <li><Link to="Technos" smooth={true} spy={true} duration={100} className="contents">Technos I use</Link></li>
+                        <li><Link to="AboutMe" smooth={true} spy={true} duration={100} className="contents">About me</Link></li>
+                        <li><Link to={Pdf} smooth={true} spy={true} duration={100} className="contents" target="_blank" rel="noreferrer">Resume</Link></li>
+                        <li><a href="#contact" className="contents">Contact</a></li>            
+                    </ul>
+                </div>
             </div>
-            <div id="contentsTable">
-            <h2>Contents</h2>
-                <ul id="contentsList">
-                    <li><a href="#projects" className="contents">Projects</a></li>
-                    <li><a href="#technos" className="contents">Techno I use</a></li>
-                    <li><a href="#AboutMe" className="contents">About me</a></li>
-                    <li><a href={Pdf} className="contents" target="blank">Resume</a></li>
-                    <li><a href="#contact" className="contents">Contact</a></li>            
-                </ul>
+            <div id="imgWrap">
+                <img  id="ppPresentation" src={pp} alt="profil"/>
             </div>
-        </div>
-        <div id="imgWrap">
-            <img  id="ppPresentation" src={pp} alt="profil"/>
         </div>
     </React.Fragment>
     )
